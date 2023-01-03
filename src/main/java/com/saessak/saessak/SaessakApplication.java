e59068ba2c65cf3b8c05f3941a60638b4e99a36e
yoon_dahoon <111041826+dotori96@users.noreply.github.com>
2023-01-03 20:14:03 +0900

    Initial commit

---
+++ src/main/java/com/saessak/saessak/SaessakApplication.java
@@ -0,0 +1,13 @@
+package com.saessak.saessak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaessakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaessakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saessak/saessak/SaessakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saessak.saessak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaessakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
