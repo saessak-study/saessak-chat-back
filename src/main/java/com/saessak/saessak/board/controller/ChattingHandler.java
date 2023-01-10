09984ec6017794422a1e322bba1bbce36993f467
Midan2047 <jeff112630@gmail.com>
2023-01-10 18:54:19 +0900

    간단 웹소켓 추가

---
+++ src/main/java/com/saessak/saessak/board/controller/ChattingHandler.java
@@ -0,0 +1,34 @@
+package com.saessak.saessak.board.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class ChattingHandler extends TextWebSocketHandler {
+
+    private static List<WebSocketSession> list = new ArrayList<WebSocketSession>();
+
+    @Override
+    public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        for(WebSocketSession sess : list) {
+            if(sess != session)
+                sess.sendMessage(message);
+        }
+    }
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        list.add(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        list.remove(session);
+    }
+}
